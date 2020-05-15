adee725d62b5d3a13e674252c4b2f2561b3a2253
Patrick Sime <psime86@gmail.com>
2020-05-15 15:51:20 -0500

    everything but the modal works.....

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,21 @@
-var friendArray =[
+var friends =[
     {
-        name: "Taako"
+        name: "Taako",
+        photo: "https://i.imgur.com/XtG79eK.png",
+        score: [5, 3, 1, 5, 3, 5, 5, 3, 2, 3]
     },
     
     {
-        name: "Merle"
+        name: "Merle",
+        photo: "https://www.nicepng.com/png/full/90-903113_taz-merle-highchurch-adventure-zone-merle-highchurch.png",
+        score: [3, 4, 5, 3, 1, 2, 4, 1, 5, 4]
     },
 
     {
-        name: "Magnus"
+        name: "Magnus",
+        photo: "https://vignette.wikia.nocookie.net/theadventurezone/images/f/fd/Magnus_by_0re.png/revision/latest?cb=20170502173448",
+        score: [2, 3, 3, 2, 2, 2, 3, 2, 3, 4]
     }
-]
+];
 
-module.exports = friendArray;
+module.exports = friends;
--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 //Dependencies:
 
 var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
 
-//Express var:
+//Vars:
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+//Body parser and Express
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("app/public"));
+
 //Routs:
 
-require("./app/routing/apiRouts.js");
-require("./app/routing/htmlRouts");
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+require("./app/routing/apiRouts.js")(app);
+require("./app/routing/htmlRouts")(app);
 
 
 app.listen(PORT, function(){
